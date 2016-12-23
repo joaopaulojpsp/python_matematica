4c11f97a339f91f47c6cb2c38ca493d0a0f5739d
= <=>
2016-12-23 16:28:41 -0200

    Adicionado funcao naturaiz com zero e sem o zero

--- conjuntos.py
+++ conjuntos.py
@@ -1,4 +1,29 @@
-# conjuntos
+# Conjuntos
 
-def naturaiz():
+# Conjuntos dos numeros naturais
+def naturais_sem_zero():
+	n = 1
+	print("Conjuntos do N(Naturais sem Zero)")
+	while(n < 15):
+		print(n)
+		n = n + 1
+
+		
+#--------------------------------------------
+		
+# Conjuntos dos numeros naturais
+def naturais_com_zero():
+	n = 0	
+	print("Conjuntos do N*(Naturais com Zero)")
+	while(n < 15):
+		print(n)
+		n = n + 1
 	
+print("1 para naturaiz sem o 0")
+print("0 para naturaiz com o 0")
+option = input()
+if(option == 1):
+	naturais_sem_zero()
+elif(option == 0):
+	naturais_com_zero()
+
