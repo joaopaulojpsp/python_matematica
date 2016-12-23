3af44207e4040f9044f48e5b10ae70ff5be7b13a
= <=>
2016-12-23 16:46:32 -0200

    Arquivo responsavel por conjuntos inteiros (Z)

---
+++ conjuntos-inteiros.py
@@ -0,0 +1,12 @@
+# Conjuntos dos numeros inteiros Z
+
+def numeros_inteiros():
+	print("Numeros inteiros(Z): ")
+	z = -14;
+	print("...")
+	while(z < 15):
+		print(z)
+		z = z + 1
+	print("...")
+
+numeros_inteiros()
