dd4d0159843791dd58e29dc54d7369f9261a8fbd
= <=>
2016-12-24 08:17:48 -0200

    Opções adicionadas

--- conjuntos-inteiros.py
+++ conjuntos-inteiros.py
@@ -34,4 +34,36 @@ def numeros_inteiros_positivos_sem_zero():
 		z = z + 1
 	print("...")
 	
-numeros_inteiros_positivos_sem_zero()
+def numeros_inteiros_negativos_sem_zero():
+	print("Numeros Inteiros Negativos Sem o Zero(Z*-): ")
+	z = -14;
+	print("...")
+	while(z < 0):
+		print(z)
+		z = z + 1
+
+def options():
+	print("Digite um numero: ")
+	print("1- Todos os Numeros Inteiros (Z)")
+	print("2- Todos os Numeros Inteiros Positivos (Z+)")
+	print("3- Todos os Numeros Inteiros  Negativos (Z-)")
+	print("4- Todos os Numeros Inteiros Positivos Sem o Zero (Z*+)")
+	print("5- Todos os Numeros Inteiros Negativos Sem o Zero (Z*-)")
+	print("6- Para Sair")
+	option = input()
+	return option
+
+option = 7
+while(option != 6):
+	if(option == 1):
+		numeros_inteiros()
+	elif(option == 2):
+		numeros_inteiros_positivos()
+	elif(option == 3):
+		numeros_inteiros_negativos()
+	elif(option == 4):
+		numeros_inteiros_positivos_sem_zero()
+	elif(option == 5):
+		numeros_inteiros_negativos_sem_zero()
+
+	option = options()
