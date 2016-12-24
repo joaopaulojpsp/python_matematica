4f0728879f10cc4fc03af6e04fe41e097a055e8f
= <=>
2016-12-24 07:51:45 -0200

    Novas Funções adicionadas!

--- conjuntos-inteiros.py
+++ conjuntos-inteiros.py
@@ -8,5 +8,30 @@ def numeros_inteiros():
 		print(z)
 		z = z + 1
 	print("...")
+	
+def numeros_inteiros_positivos():
+	print("Numeros Inteiros Positivos(Z+): ")
+	z = 0;
+	while(z < 15):
+		print(z)
+		z = z + 1
+	print("...")
+	
+def numeros_inteiros_negativos():
+	print("Numeros Inteiros Negativos(Z-): ")
+	z = -14;
+	print("...")
+	while(z < 1):
+		print(z)
+		z = z + 1
+	print("...")
 
-numeros_inteiros()
+def numeros_inteiros_positivos_sem_zero():
+	print("Numeros Inteiros Positivos sem o zero(Z*+): ")
+	z = 1;
+	while(z < 15):
+		print(z)
+		z = z + 1
+	print("...")
+	
+numeros_inteiros_positivos_sem_zero()
